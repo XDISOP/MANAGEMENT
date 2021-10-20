62171904e5ff4c850242844cf258b55e245ac929
STBxD <87096507+STBxD@users.noreply.github.com>
2021-10-20 16:57:59 +0530

    Update __main__.py

--- Natsuki/__main__.py
+++ Natsuki/__main__.py
@@ -63,7 +63,7 @@ from Natsuki.modules.helper_funcs.misc import paginate_modules
 from Natsuki.modules.helper_funcs.readable_time import get_readable_time
 
 PM_START_TEXT = """
-👋 Hey There, My Name is Luffy. 
+👋 Hey There, My Name is [Luffy](https://telegra.ph/file/569aa1e3bf22d56900605.jpg). 
 I'm a Powerfull Group Manager Bot With Cool Modules. feel free to add me to your groups!
 """
 
