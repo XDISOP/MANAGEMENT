ac2fcf8c0f66562fc9aa1a3cf1e65b1834bc5ed7
Kazama009 <kazama009@protonmail.com>
2022-05-18 19:32:44 +0530

    fix tagger
    also update ForceSubscribe to pyrogram v2.0, also update requirements.txt

--- Natsuki/modules/ForceSubscribe.py
+++ Natsuki/modules/ForceSubscribe.py
@@ -83,7 +83,7 @@ def _onUnMuteRequest(client, cb):
                 )
 
 
-@pbot.on_message(filters.text & ~filters.private & ~filters.edited, group=1)
+@pbot.on_message(filters.text & ~filters.private, group=1)
 def _check_member(client, message):
     chat_id = message.chat.id
     chat_db = sql.fs_settings(chat_id)
@@ -138,7 +138,7 @@ def _check_member(client, message):
 @pbot.on_message(filters.command(["forcesubscribe", "fsub"]) & ~filters.private)
 def config(client, message):
     user = client.get_chat_member(message.chat.id, message.from_user.id)
-    if user.status is "creator" or user.user.id in SUDO_USERS:
+    if user.status == "creator" or user.user.id in SUDO_USERS:
         chat_id = message.chat.id
         if len(message.command) > 1:
             input_str = message.command[1]
